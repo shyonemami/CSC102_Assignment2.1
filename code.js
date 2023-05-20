5e89f6ed10ac9c086d2cac421dac1569e4254682
shyonemami <133165861+shyonemami@users.noreply.github.com>
2023-05-20 16:20:32 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,28 @@
+function restart()
+    // Generates two random numbers beween 1 and 10
+{   var number1  = Math.ceil(Math.random() *10);
+    var number2  = Math.ceil(Math.random() *10);
+    // Calculates the sum of two random numbers
+    var sum = number1 + number2;
+    // Checks if number1 is even and equal to number2
+    if (number1 % 2 == 0  && number1 == number2)
+    {
+        // Displays game result for WIN if the above (if) statement is true
+        document.getElementById("result").innerHTML = "Even and Equal: YOU WIN"
+    }
+    // Checks if number2 is 6 or the sum of both numbers is 6
+    else if (number2 == 6 || sum == 6)
+    {
+        // Displays game result for WIN if the above (else if) statement is true
+        document.getElementById("result").innerHTML = "number2 = 6 or sum = 6: YOU WIN"
+    }
+    else
+    {
+        // Displays game result for LOSS if the (if/else if) statements above are false
+        document.getElementById("result").innerHTML = "Conditions for win not met: YOU LOSE"
+    }
+    // Displays the value of number1
+    document.getElementById("number1").innerHTML = "number1 = " + number1;
+    // Displays the value of number2
+    document.getElementById("number2").innerHTML = "number2 = " + number2;
+}
